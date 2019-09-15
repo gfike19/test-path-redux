1e3f5b0e4167c97759782ba5d6fae98ee9477763
gfike19@gmail.com <Vaughn*573>
2019-09-15 14:12:39 -0500

    first commit

---
+++ src/main/java/com/gfike/testpathredux/TestPathReduxApplication.java
@@ -0,0 +1,13 @@
+package com.gfike.testpathredux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPathReduxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPathReduxApplication.class, args);
+	}
+
+}
